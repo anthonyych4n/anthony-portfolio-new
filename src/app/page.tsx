4260bdc508267fb31db666bfe0205cfbde33933d
Anthony Chan <anthonyych4n@gmail.com>
2024-11-19 02:36:40 -0700

    scroll finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <div className='flex items-center justify-center h-full bg-neutral-900 space-x-20 relative'>
-      <div className='min-w-30 h-screen fixed'>
-        <About />
-        {/*<Animation />*/}
-      </div>
-      <div className='min-w-30 scroll-smooth space-y-20'>
-        <Background />
-        <Footer />
+    <div className='flex justify-center min-h-screen bg-neutral-900'>
+      <div className='w-full max-w-7xl flex relative'>
+        <div className='fixed ml-[200px] w-[500px] pt-40'>
+          <About />
+          {/*<Animation />*/}
+        </div>
+        <div className='ml-[700px] flex-1 pt-40 '>
+          <div className='flex flex-col space-y-20'>
+            <Background />
+            <Footer />
+          </div>
+        </div>
       </div>
     </div>
   );
